e0dab9c9e490c31f895f7b9180439ce24c1f9646
deng <978548481@qq.com>
2016-12-22 22:40:26 +0800

    发布完成

---
+++ app/src/main/java/com/project/zhinan/bean/FBBean.java
@@ -0,0 +1,105 @@
+package com.project.zhinan.bean;
+
+import java.util.List;
+
+/**
+ * Created by deng on 16-12-22.
+ */
+
+public class FBBean {
+
+    /**
+     * name : 123123
+     * imgurls[] : ["http://guide-10035266.image.myqcloud.com/guide1482412503557","http://guide-10035266.image.myqcloud.com/guide1482412517443"]
+     * key : 123123
+     * ad_put_begintime : 2016-12-22
+     * ad_put_endtime : 2016-12-23
+     * budget : 122222
+     * sig_money : 123
+     * tags[] : ["jiaoyu","lvyou"]
+     * addesc : 快来买呀！便宜处理了
+     */
+
+    private String name;
+    private String key;
+    private String ad_put_begintime;
+    private String ad_put_endtime;
+    private String budget;
+    private String sig_money;
+    private String addesc;
+
+    public List<String> getImgurls() {
+        return imgurls;
+    }
+
+    public void setImgurls(List<String> imgurls) {
+        this.imgurls = imgurls;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public java.util.List<String> imgurls;
+    public java.util.List<String> tags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getAd_put_begintime() {
+        return ad_put_begintime;
+    }
+
+    public void setAd_put_begintime(String ad_put_begintime) {
+        this.ad_put_begintime = ad_put_begintime;
+    }
+
+    public String getAd_put_endtime() {
+        return ad_put_endtime;
+    }
+
+    public void setAd_put_endtime(String ad_put_endtime) {
+        this.ad_put_endtime = ad_put_endtime;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getSig_money() {
+        return sig_money;
+    }
+
+    public void setSig_money(String sig_money) {
+        this.sig_money = sig_money;
+    }
+
+    public String getAddesc() {
+        return addesc;
+    }
+
+    public void setAddesc(String addesc) {
+        this.addesc = addesc;
+    }
+}
--- app/src/main/java/com/project/zhinan/fragment/FabuFragment.java
+++ app/src/main/java/com/project/zhinan/fragment/FabuFragment.java
@@ -19,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -31,13 +32,16 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
+import com.google.gson.Gson;
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.loader.GlideImageLoader;
 import com.lzy.imagepicker.ui.ImageGridActivity;
 import com.lzy.imagepicker.view.CropImageView;
+import com.project.zhinan.MyApplication;
 import com.project.zhinan.R;
 import com.project.zhinan.activity.LoginActivity;
+import com.project.zhinan.bean.FBBean;
 import com.project.zhinan.utils.ConstantValue;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -47,6 +51,12 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+import com.tencent.upload.Const;
+import com.tencent.upload.UploadManager;
+import com.tencent.upload.task.ITask;
+import com.tencent.upload.task.IUploadTaskListener;
+import com.tencent.upload.task.data.FileInfo;
+import com.tencent.upload.task.impl.PhotoUploadTask;
 import com.vincent.filepicker.Constant;
 import com.vincent.filepicker.activity.ImagePickActivity;
 import com.vincent.filepicker.filter.entity.ImageFile;
@@ -61,7 +71,12 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 import static android.app.Activity.RESULT_OK;
 import static com.vincent.filepicker.Util.getScreenWidth;
@@ -74,6 +89,12 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
 
 
     private static final int IMAGECHANGED = 1;
+    private static final int MAXIMGNUM = 5;
+    private static final int IMAGEUPLOADED = 2;
+    private static final int UPLOADSUCCESS = 3;
+    private static final int UPLOADFAIL = 4;
+    private static final int JSONERROR = 5;
+    private static final int NETERROR = 6;
     private EditText mAdNameEditText;
     private LinearLayout mImgContainerLinearLayout;
     private ImageButton mAddImgImageButton;
@@ -93,6 +114,17 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
     public String starttime;
     public ArrayList<String> imgList;
     private Handler handler;
+    private Button mClearImg;
+    private LinearLayout mTime;
+    private String mAdname;
+    private String mKeywords;
+    private String mAdStart;
+    private String mAdEnd;
+    private String mAdBuget;
+    private String mAdSigleMoney;
+    private ArrayList<String> UploadimgUrls;
+    private ArrayList<String> mCheckList;
+    private ProgressBar mProgress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -117,8 +149,13 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
         mFabuButton.setOnClickListener(this);
         mEditLinearLayout = (LinearLayout) view.findViewById(R.id.ll_edit);
         mEditScrollView = (ScrollView) view.findViewById(R.id.sv_edit);
+        mClearImg = (Button) view.findViewById(R.id.clear_img);
+        mClearImg.setOnClickListener(this);
+        mTime = (LinearLayout) view.findViewById(R.id.time);
+        mProgress = (ProgressBar) view.findViewById(R.id.fabu_progress);
+        imgList = new ArrayList<>();
+        UploadimgUrls = new ArrayList<>();
 
-        imgList=new ArrayList<>();
         return view;
     }
 
@@ -135,7 +172,6 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
         return view;
 
     }
-
 
 
     @Override
@@ -157,19 +193,82 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
                             mImgContainerLinearLayout.addView(addView(path));
                         }
                         break;
+                    case IMAGEUPLOADED:
+                        infoupload();
+                        break;
+                    case UPLOADSUCCESS:
+                        toast("上传成功");
+                        dissMissProgress();
+                        initData();
+                        break;
+                    case UPLOADFAIL:
+                        toast("上传失败");
+                        dissMissProgress();
+                        break;
+                    case JSONERROR:
+                        toast("JSON解析错误");
+                        dissMissProgress();
+                        break;
+                    case NETERROR:
+                        toast("网络错误");
+                        dissMissProgress();
+                        break;
+                    default:
+                        dissMissProgress();
+                        break;
                 }
             }
         };
+    }
+
+    private void dissMissProgress() {
+        mProgress.setVisibility(View.GONE);
+    }
+
+    private void showProgress() {
+        mProgress.setVisibility(View.VISIBLE);
+    }
+
+
+    private void initData() {
+        mProgress.setVisibility(View.GONE);
+        mAdNameEditText.setText("");
+        mImgContainerLinearLayout.removeAllViews();
+        mAdKeyEditText.setText("");
+        mStartTimeTextView.setText("");
+        mStartTimeTextView.setHint("开始时间");
+        mEndTimeTextView .setHint("结束时间");
+        mEndTimeTextView .setText("");
+        mBudgetEditText.setText("");
+        mSigMoneyEditText .setText("");
+        CheckBox[] cbs = {mEduCheckBox,
+                mTourCheckBox,
+                mBuildCheckBox,
+                mMedCheckBox,
+                mFoodCheckBox};
+        for (CheckBox cb : cbs) {
+            cb.setChecked(false);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        handler=null;
+        super.onDestroy();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add_img:
-                Intent intent1 = new Intent(getActivity(), ImagePickActivity.class);
-                intent1.putExtra(IS_NEED_CAMERA, true);
-                intent1.putExtra(Constant.MAX_NUMBER, 5);
-                startActivityForResult(intent1, Constant.REQUEST_CODE_PICK_IMAGE);
+                if (MAXIMGNUM - imgList.size() > 0) {
+                    Intent intent1 = new Intent(getActivity(), ImagePickActivity.class);
+                    intent1.putExtra(IS_NEED_CAMERA, true);
+                    intent1.putExtra(Constant.MAX_NUMBER, MAXIMGNUM - imgList.size());
+                    startActivityForResult(intent1, Constant.REQUEST_CODE_PICK_IMAGE);
+                } else {
+                    Toast.makeText(getContext(), "最多上传" + MAXIMGNUM + "张图片", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.start_time:
                 TimeSelector timeSelector = new TimeSelector(getContext(), new TimeSelector.ResultHandler() {
@@ -198,9 +297,170 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
                     timeSelectorend.show();
                 }
                 break;
+            case R.id.clear_img:
+                imgList.clear();
+                UploadimgUrls.clear();
+                mImgContainerLinearLayout.removeAllViews();
+                break;
             case R.id.bt_fabu:
+                if (isLogin() && infoCheck()) {
+                    uploadfile();
+                }
                 break;
         }
+    }
+
+    private void infoupload() {
+        new Thread() {
+            @Override
+            public void run() {
+                SharedPreferences cookie = getActivity().getSharedPreferences("cookie", Context.MODE_PRIVATE);
+                String my_cookie = cookie.getString("my_cookie", "");
+                FBBean fbBean = new FBBean();
+                fbBean.setName(mAdname);
+                fbBean.setAd_put_begintime(mAdStart);
+                fbBean.setAd_put_endtime(mAdEnd);
+                fbBean.setBudget(mAdBuget);
+                fbBean.setSig_money(mAdSigleMoney);
+                fbBean.setImgurls(UploadimgUrls);
+                fbBean.setKey(mKeywords);
+                fbBean.setTags(mCheckList);
+                OkHttpClient client = new OkHttpClient();
+                MediaType mediaType = MediaType.parse("application/json");
+                Gson gson = new Gson();
+                RequestBody body = RequestBody.create(mediaType, gson.toJson(fbBean));
+                Request request = new Request.Builder()
+                        .url("http://123.206.84.242:2888/mpostNew")
+                        .post(body)
+                        .addHeader("content-type", "application/json")
+                        .addHeader("cache-control", "no-cache")
+                        .addHeader("cookie", my_cookie)
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String string = response.body().string();
+                    System.out.println(string);
+                    if (string.contains("success")) {
+                        handler.sendEmptyMessage(UPLOADSUCCESS);
+                    } else {
+                        handler.sendEmptyMessage(UPLOADFAIL);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    handler.sendEmptyMessage(NETERROR);
+                }
+            }
+        }.start();
+
+
+    }
+
+
+    private boolean infoCheck() {
+        mAdname = mAdNameEditText.getText().toString().trim();
+        mKeywords = mAdKeyEditText.getText().toString().trim();
+        mAdStart = mStartTimeTextView.getText().toString().trim();
+        mAdEnd = mEndTimeTextView.getText().toString().trim();
+        mAdBuget = mBudgetEditText.getText().toString().trim();
+        mAdSigleMoney = mSigMoneyEditText.getText().toString().trim();
+        return isNameValid(mAdname) && isKeywordsValid(mKeywords) && isTimeValid(mAdStart, mAdEnd) && isBugetValid(mAdBuget) && isSigleMoneyValid(mAdSigleMoney) && isCkValid();
+    }
+
+    private boolean isCkValid() {
+        CheckBox[] cbs = {mEduCheckBox,
+                mTourCheckBox,
+                mBuildCheckBox,
+                mMedCheckBox,
+                mFoodCheckBox};
+//        String[] tags = {"教育", "旅游", "建材","医药","餐饮"};
+        mCheckList = new ArrayList<String>();
+        for (CheckBox cb :
+                cbs) {
+            if (cb.isChecked()) {
+                mCheckList.add(cb.getText().toString());
+            }
+        }
+        if (mCheckList.size() < 1) {
+            toast("请至少选择一个分类");
+            return false;
+        }
+        if (mCheckList.size() > 3) {
+            toast("最多只能选三个分类");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isSigleMoneyValid(String mAdSigleMoney) {
+        if (isInteger(mAdSigleMoney)) {
+            if (Integer.parseInt(mAdSigleMoney) < 0) {
+                mSigMoneyEditText.requestFocus();
+                toast("请输入大于零的整数");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        mSigMoneyEditText.requestFocus();
+        toast("请输入整数");
+        return false;
+    }
+
+    private boolean isBugetValid(String mAdBuget) {
+        if (isInteger(mAdBuget)) {
+            if (Integer.parseInt(mAdBuget) < 0) {
+                mBudgetEditText.requestFocus();
+                toast("请输入大于零的整数");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        mBudgetEditText.requestFocus();
+        toast("请输入整数");
+        return false;
+
+    }
+
+    private boolean isInteger(String str) {
+        if (null == str || "".equals(str)) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
+
+    private boolean isTimeValid(String mAdStart, String mAdEnd) {
+        if (mAdStart.length() > 0 && mAdEnd.length() > 0)
+            return true;
+        Toast.makeText(getContext(), "请选择正确的时间", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private boolean isKeywordsValid(String mKeywords) {
+        if (mKeywords.length() >= 4 && mKeywords.length() <= 10)
+            return true;
+        mAdKeyEditText.requestFocus();
+        toast("关键词长度错误");
+        return false;
+    }
+
+    private boolean isNameValid(String mAdname) {
+        if (mAdname.length() > 0)
+            return true;
+        mAdNameEditText.requestFocus();
+        toast("广告名称未填写");
+        return false;
+    }
+
+    private void toast(String s) {
+        Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isLogin() {
+        if (!MyApplication.getInstance().isLogin()) toast("还未登陆");
+
+        return MyApplication.getInstance().isLogin();
     }
 
     public String getFormatStartDate() {
@@ -226,25 +486,76 @@ public class FabuFragment extends Fragment implements View.OnClickListener {
             case Constant.REQUEST_CODE_PICK_IMAGE:
                 if (resultCode == RESULT_OK) {
                     ArrayList<ImageFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_IMAGE);
-
+                    mImgContainerLinearLayout.removeAllViews();
                     for (ImageFile file : list) {
                         String path = file.getPath();
                         imgList.add(path);
                     }
-                    handler.sendEmptyMessage(IMAGECHANGED);
-//                    gridViewPhoto.notifyDataSetChanged();
+                    for (String path :
+                            imgList) {
+                        mImgContainerLinearLayout.addView(addView(path));
+                    }
                 }
                 break;
-            case Constant.REQUEST_CODE_PICK_VIDEO:
-                if (resultCode == RESULT_OK) {
-                    ArrayList<VideoFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_VIDEO);
-//                    for (VideoFile file : list) {
-//                        String path = file.getPath();
-//                        listvideo.add(path);
-//                        gridViewVideo.notifyDataSetChanged();
-//                    }
+        }
+    }
+
+    public void uploadfile() {
+        showProgress();
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < imgList.size(); i++) {
+            final int finalI = i;
+            singleThreadExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    UploadManager guide = new UploadManager(getContext(), "10035266", Const.FileType.Photo, null);
+                    PhotoUploadTask uploadTask = new PhotoUploadTask(imgList.get(finalI), new IUploadTaskListener() {
+                        @Override
+                        public void onUploadSucceed(FileInfo fileInfo) {
+                            System.out.println("----------------" + fileInfo.toString() + "--------" + fileInfo.url);
+                            UploadimgUrls.add(fileInfo.url);
+                            if (finalI == imgList.size() - 1) {
+                                handler.sendEmptyMessage(IMAGEUPLOADED);
+                            }
+                        }
+
+                        @Override
+                        public void onUploadFailed(int i, String s) {
+                            System.out.println("----------------" + s);
+                        }
+
+                        @Override
+                        public void onUploadProgress(long l, long l1) {
+                            System.out.println(l + "----------------" + l1);
+                        }
+
+                        @Override
+                        public void onUploadStateChange(ITask.TaskState taskState) {
+                            System.out.println("----------------" + taskState);
+                        }
+                    });
+                    OkHttpClient client = new OkHttpClient();
+
+                    Request request = new Request.Builder()
+                            .url("http://123.206.84.242:2888/tx/img")
+                            .get()
+                            .addHeader("cache-control", "no-cache")
+                            .build();
+                    try {
+                        Response response = client.newCall(request).execute();
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        System.out.println(jsonObject.getString("url") + "========================================");
+                        uploadTask.setBucket("guide");
+                        uploadTask.setFileId("guide" + UUID.randomUUID());
+                        uploadTask.setAuth(jsonObject.getString("sign"));
+                        guide.upload(uploadTask);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-                break;
+            });
         }
     }
 }
